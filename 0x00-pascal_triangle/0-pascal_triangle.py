d7f6ef35eebce97c6ff7fb6161f0f1281c4b828f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-07 17:46:56 +0300

    efficient

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,15 +6,15 @@ def pascal_triangle(n):
     if n <= 0:
         yield []  # Return an empty row if n is less than or equal to 0
     else:
-        previous_row = []
-        for idx in range(n):
+        previous_row = [1]
+        yield previous_row
+        
+        for idx in range(1, n):
             # Create the current row with 1s
             row = [1] * (idx + 1)
             # Update the middle elements based on the previous row
-            row = [
-                row[i] if i == 0 or i == idx else previous_row[i - 1] + previous_row[i]
-                for i in range(idx + 1)
-            ]
+            row[1:idx] = [previous_row[i - 1] + previous_row[i]
+                          for i in range(1, idx)]
             yield row
             previous_row = row
 
