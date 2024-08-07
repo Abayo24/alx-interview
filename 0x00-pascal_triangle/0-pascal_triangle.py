274ed0a93f98ef3e59b1b5e580c29952f90cf481
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-07 17:30:12 +0300

    0. Pascals Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,23 +2,14 @@
 """pascals Triangle
 """
 def pascal_triangle(n):
-    triangle = []
-    if (n <= 0):
-        return triangle
-    
-    for idx in range(n):
-        row = []
-        for i in range(n+1):
-            binom_coeff = factorial(idx) // (factorial(i) * factorial(idx - i))
-            row.append(binom_coeff)
-        triangle.append(row)
-        
-    return triangle
+    if n <= 0:
+        yield []
+    else:
+        for idx in range(n):
+            triangle = [factorial(idx) // (factorial(i) * factorial(idx - i)) for i in range(idx + 1)]
+            yield triangle
 
 def factorial(num):
     if num == 0:
         return 1
-    result = 1
-    for k in range(1, num+1):
-        result *= k
-    return result
+    return num * factorial(num - 1)
