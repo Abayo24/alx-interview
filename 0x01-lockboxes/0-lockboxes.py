3828b2339561e3b6548aeeaaf8541063ef8c73c7
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-15 10:28:47 +0300

    readme.md

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,12 +6,12 @@ itetate through list:
     if elem is list:
         iterate list:
             if elem <= lis1.length and elem not in newList:
-                newList.append(elem) 
+                newList.append(elem)
             else throw error
     else not list:
         if elem <= lis1.length and elem not in newList:
                 newList.append(elem)
-    if elem in newList 
+    if elem in newList
 method that determines if all the boxes can be opened
 """
 
@@ -20,11 +20,12 @@ def canUnlockAll(boxes):
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
-    
+
     keys = boxes[0]
     for key in keys:
         if key < n and not unlocked[key]:
             unlocked[key] = True
             keys.extend(boxes[key])
-    
+
     return all(unlocked)
+  
