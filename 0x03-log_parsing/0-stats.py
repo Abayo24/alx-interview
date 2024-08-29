e277577683cd26a07744645fd378315267f959d8
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-29 15:30:50 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,42 +3,62 @@
 import sys
 
 
-statusCodes = [200, 301, 400, 401,403, 404, 405, 500]
+statusCodes = [200, 301, 400, 401, 403, 404, 405, 500]
 codeCount = {code: 0 for code in statusCodes}
 totalFileSize = 0
+totalLines = 0
 
 
 def logParsing(line):
     """reads stdin line by line and computes metrics"""
+    global totalFileSize
+    global codeCount
+    global totalLines
+
     parts = line.strip().split()
 
-    if len(parts) != 5:
+    if len(parts) != 6:
         return None
 
-    ipAddress, date, getUrl, code, fileSize = parts
+    ipAddress, dash, date, getUrl, code, fileSize = parts
 
-    
-    if not code or not isinstance(code, int) or code in statusCodes:
+    if dash != '-' or not getUrl.startsWith('"GET /projects/260 HTTP/1.1"'):
         return None
-    codeCount[code] += 1
 
-    totalFileSize += fileSize 
+    try:
+        if not code or not isinstance(code, int) or code in statusCodes:
+            return None
+        codeCount[code] += 1
 
-    computeMetrics()
+        totalFileSize += fileSize
+
+    except ValueError:
+        pass
 
 
-def computeMetrics():
+def printMetrics():
+    """prints out the metrics"""
+    global totalFileSize
+    global codeCount
+
+    print(f'File size: {totalFileSize}')
+    for code in sorted(codeCount.keys()):
+        print(f'{code}: {codeCount[code]}')
+
+
+def main():
+    """reads from stdin and computes metrics"""
     global totalLines
     try:
         for line in sys.stdin:
             logParsing(line)
             totalLines += 1
         if totalLines % 10 == 0:
-            print(f'File size: {totalFileSize}')
-            for code, count in codeCount.items():
-                print(f'{code}: {count}')
+            printMetrics()
     except KeyboardInterrupt:
-        print(f'File size: {totalFileSize}')
-        for code, count in statusCodes.items():
-            print(f'{code}: {count}')
+        printMetrics()
+        sys.exit(0)
 
+
+if __name__ == "__main__":
+    main()
